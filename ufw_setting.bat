86cdce5e5109193bc5676a127ee4daea0af2c2ef
Yeosangho <soboru963@ajou.ac.kr>
2018-02-21 11:56:27 +0900

    dd

--- ufw_setting.bat
+++ ufw_setting.bat
@@ -1,19 +1,16 @@
-sudo apt-get --yes --force-yes install ufw 
-
+sudo apt-get --yes --force-yes install ufw
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow https
 sudo ufw allow http
-sudo ufw allow from 210.107.0.0/24
-
+sudo ufw allow from 210.107.197.0/24
 if [ $(whoami) = "lovelace" ] ; then
-        echo "user $(whoami)"
-	sudo ufw allow ssh
-	sudo ufw allow ftp
+echo "user $(whoami)"
+sudo ufw allow ssh
+sudo ufw allow ftp
+sudo ufw allow 2222/tcp
 else
-        echo "user $(whoami) is not lovelcace"
-	
+echo "user $(whoami) is not lovelcace"
 fi
-
-sudo ufw enable 
+sudo ufw enable
 sudo ufw status verbose
